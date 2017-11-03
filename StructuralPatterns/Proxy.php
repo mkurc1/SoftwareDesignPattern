98b7a589105a5f1b668fdf6e15cae29e6c8bfbc6
Michał Kurcewicz <mkurc1@gmail.com>
2017-11-03 18:41:47 +0100

    Added proxy pattern

---
+++ StructuralPatterns/Proxy.php
@@ -0,0 +1,53 @@
+<?php
+
+interface Image
+{
+	public function render();
+}
+
+class RealImage
+{
+	private $filename;
+
+	public function __construct($filename)
+	{
+		$this->filename = $filename;
+		$this->loadImage();
+	}
+
+	private function loadImage()
+	{
+		echo 'Load image: ' . $this->filename . '<br>';
+	}
+
+	public function render()
+	{
+		echo 'Image content: ' . $this->filename . '<br>';
+	}
+}
+
+class ProxyImage implements Image
+{
+	private $filename;
+	private $image;
+
+	public function __construct($filename)
+	{
+		$this->filename = $filename;
+	}
+
+	public function render()
+	{
+		if (null === $this->image) {
+			$this->image = new RealImage($this->filename);
+		}
+
+		return $this->image->render();
+	}
+}
+
+$image = new ProxyImage('image_big');
+echo $image->render();
+echo $image->render();
+
+?>
